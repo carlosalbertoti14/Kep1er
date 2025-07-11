3dffeef6895e9edaa1ddb56bfa39d9d60159013d
Carlos Alberto <c.alberto.ti14@gmail.com>
2025-07-11 19:57:53 -0300

    update

--- midia/listar_arquivos.bat
+++ midia/listar_arquivos.bat
@@ -2,10 +2,20 @@
 setlocal
 
 set "outputFile=Nomes_dos_Arquivos.txt"
+set "tempFile=temp_file_list.txt"
 
 echo Listando arquivos na pasta atual...
-dir /b /a-d > "%outputFile%"
+
+:: Get all files and redirect to a temporary file
+dir /b /a-d > "%tempFile%"
+
+:: Filter out the specified files and write to the final output file
+findstr /V /I /L "%~nx0" "%tempFile%" | findstr /V /I /L "%outputFile%" > "%outputFile%"
+
+:: Clean up the temporary file
+del "%tempFile%"
 
 echo.
 echo Nomes dos arquivos salvos em "%outputFile%".
-pause
+
+endlocal
